c80ff74dfe88f2b79baea6080a1bf865da62b039
suika05 <dev.suika05@gmail.com>
2023-01-01 18:08:17 +0900

    実装ファイル追加

---
+++ omikuji.js
@@ -0,0 +1,29 @@
+$body = $('body');
+
+let resultList = [
+  '大吉','吉','中吉','小吉','末吉','凶'
+];
+
+let resultItem;
+
+function resultNum() {
+  let minNum = 0;
+  let maxNum = resultList.length - 1;
+
+  let randomNum = Math.floor(Math.random() * (maxNum + 1 - minNum)) + minNum;
+
+  $('.js-result-item').html(resultList[randomNum]);
+  resultItem = resultList[randomNum];
+}
+function shareLink () {
+  let resultText = resultItem;
+  let urlLink = 'https://twitter.com/intent/tweet?text=あなたのおみくじの結果は'+ resultText +'！';
+
+  $('.js-result-share').attr('href',urlLink);
+}
+
+$('.js-start-btn').on('click', function () {
+  $body.addClass('is-start');
+  resultNum();
+  shareLink();
+});
